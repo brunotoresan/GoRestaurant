f621c8ee59eefa7d8e43526d63feb1f8ff31cf2c
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-07-06 07:53:14 -0300

    fixed Header bug

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -105,7 +105,7 @@ class Dashboard extends Component {
 
     return (
       <>
-        <Header openModal={this.toggleModal} />
+        <Header onOpenNewDishModal={this.toggleModal} />
         <ModalAddFood
           isOpen={modalOpen}
           setIsOpen={this.toggleModal}
